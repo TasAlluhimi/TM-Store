6facad5ae35b7574fcf91558dee581084113776f
tasalluhimi <tasneem.luhimi@gmail.com>
2023-11-13 22:35:10 +0300

    a

--- main.js
+++ main.js
@@ -1,57 +1,50 @@
 
 
-function apiFunction() {
+// function apiFunction() {
     
-    console.log(1);
-    let cont = document.querySelector("#cont");
+//     // console.log(1);
+//     let cont = document.querySelector("#cont");
 
-    const products_api = 'https://fakestoreapi.com/products';
+//     const products_api = 'https://fakestoreapi.com/products';
     
-    fetch(products_api)
+//     fetch(products_api)
 
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+//     .then(response => response.json())
+//     .then(data => {
+//         // console.log(data);
 
-        for (let i = 0; i < data.length; i++) {
-            let box = document.createElement("div");
-            box.setAttribute("id", "box");
-            box.setAttribute("class", "box");
+//         for (let i = 0; i < data.length; i++) {
+//             let box = document.createElement("div");
+//             box.setAttribute("id", "box");
+//             box.setAttribute("class", "box");
             
-            cont.appendChild(box);
+//             cont.appendChild(box);
             
-            let para = document.createElement("p");
-            para.setAttribute("id", "para");
-            para.setAttribute("class", "para");
-            para.innerHTML = `${data[i].title} <hr>`
+//             let para = document.createElement("p");
+//             para.setAttribute("id", "para");
+//             para.setAttribute("class", "para");
+//             para.innerHTML = `${data[i].title} <hr>`
 
-            box.appendChild(para);
+//             box.appendChild(para);
 
-            let image = document.createElement("img");
-            image.setAttribute("id", "img");
-            image.setAttribute("class", "img");
-            image.setAttribute("src", data[i].image);
-            box.appendChild(image);
+//             let image = document.createElement("img");
+//             image.setAttribute("id", "img");
+//             image.setAttribute("class", "img");
+//             image.setAttribute("src", data[i].image);
+//             box.appendChild(image);
 
-            let price = document.createElement("small")
-            price.setAttribute("id", "price");
-            price.setAttribute("class", "price");
-            price.innerHTML = `<hr> Price: ${data[i].price} SAR`
-            box.appendChild(price)
+//             let price = document.createElement("small")
+//             price.setAttribute("id", "price");
+//             price.setAttribute("class", "price");
+//             price.innerHTML = `<hr> Price: ${data[i].price} SAR`
+//             box.appendChild(price)
 
-        }
-    })
-}
+//         }
+//     })
+// }
+// apiFunction();
 
 
-function logIn() {
 
-    signUpBtn = document.querySelector("#signUp");
 
-    const api = '';
-    fetch(api)
-    .then()
-    .then()
-
-}
-apiFunction();
+signUp();
