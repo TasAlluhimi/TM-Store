14aa47fac3eb3b435c7dd59fe284b332f703bd80
tasalluhimi <tasneem.luhimi@gmail.com>
2023-11-13 11:27:56 +0300

    done

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+function apiFunction() {
+    
+    let cont = document.querySelector("#cont");
+
+    const api = 'https://fakestoreapi.com/products';
+    
+    fetch(api)
+
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+            let box = document.createElement("div");
+            box.setAttribute("id", "box");
+            box.setAttribute("class", "box");
+            
+            cont.appendChild(box);
+            
+            let para = document.createElement("p");
+            para.setAttribute("id", "para");
+            para.setAttribute("class", "para");
+            para.innerHTML = `${data[i].title} <hr>`
+
+            box.appendChild(para);
+
+            let image = document.createElement("img");
+            image.setAttribute("id", "img");
+            image.setAttribute("class", "img");
+            image.setAttribute("src", data[i].image);
+            box.appendChild(image);
+
+            let price = document.createElement("small")
+            price.setAttribute("id", "price");
+            price.setAttribute("class", "price");
+            price.innerHTML = `<hr> Price: ${data[i].price} SAR`
+            box.appendChild(price)
+
+        }
+    })
+}
+
+apiFunction();
